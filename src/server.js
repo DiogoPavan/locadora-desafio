21285777f0cffd421566abf6a8f78f4282bf5ec1
Diogo Reis Pavan <diogopv10@gmail.com>
2020-07-23 12:57:06 -0300

    Criando rotas e controllers iniciais

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`Servidor rodando na porta ${process.env.APP_PORT}`);
